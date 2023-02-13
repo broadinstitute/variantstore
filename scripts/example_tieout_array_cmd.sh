7762e17e0f8df3371f56391b326c0dee5efbed0b
Aaron Hatcher <hatcher@broadinstitute.org>
2023-02-13 12:12:22 -0500

    wiping all previous data away

--- scripts/example_tieout_array_cmd.sh
+++
@@ -1,6 +0,0 @@
-F1=204126160095_R01C01.vcf
-F2=aou_gda_1.vcf
-
-cat $F1 | grep -v "#" | sort > /tmp/o1.txt
-cat $F2 | grep -v "#" | sort > /tmp/o2.txt
-python compare_array_vcfs.py /tmp/o1.txt /tmp/o2.txt
